ee5235b54ff95b4bf565a5925c3ba7740d026097
王斑 <yzbbanban@live.com>
2022-08-04 11:09:08 +0800

    top 10

---
+++ navigation/nft/NftNav2.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type AuthorItem = {
+    title: string;
+    href: string;
+    description: string;
+    imageUrl: string;
+};
+
+const navList: AuthorItem[] = require("./navList2.json");
+
+// 主页ens介绍
+export default function NftNav() {
+    return (
+        <div style={{display: 'flex', flexWrap: 'wrap', gap: '10px'}}>
+            {navList.map((collection, index) => (
+                <div key={index} style={{padding: '10px', backgroundColor: '#eeeeee22',width:'400px'}}>
+                    <a href={collection.href} target={"_blank"}>
+                        <img src={collection.imageUrl} style={{width: '400px', height: '250px'}}/>
+                        <div>{collection.title}</div>
+                        <div style={{fontSize: '14px'}}>{collection.description}</div>
+                    </a>
+                </div>
+            ))}
+        </div>
+    );
+}
--- navigation/nft/index.md
+++ navigation/nft/index.md
@@ -6,7 +6,20 @@ image: /img/bac111.png
 description: NFT相关及数据分析工具汇总
 ---
 import NftNav from './NftNav';
+import NftNav2 from './NftNav2';
 
 ![bac](./assets/bac-info12.jpg)
 
+### 通用工具
 <NftNav></NftNav>
+
+### 前10名NFT分析、使用工具
+
+我们的工具将把我们带到哪里？
+
+NFT 市场仍处于早期阶段，并有望在未来几年增长，各种日益复杂的工具进入市场，以帮助我们发现最佳的 NFT 投资。
+
+随着其他 NFT 用例的兴起，例如部分所有权、知识产权、价值存储、实物资产所有权证明和更复杂的金融工具，人们只能想象 NFT 工具的演变。但有一件事是肯定的，激动人心的时刻即将到来。
+（以下为推荐，可能会与上面列举的重复）
+
+<NftNav2></NftNav2>
